a458fcb0357600b4bc6fa92daecfd97e04754809
Tamer Vassib <tvassib@gmail.com>
2025-11-22 00:03:04 +0000

    running code

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5002,host = "192.168.1.23")
+    app.run(debug=True,port=5001,host = "127.0.0.1")
--- run_production.py
+++ run_production.py
@@ -2,6 +2,7 @@ from app import app
 from waitress import serve
 
 if __name__ == "__main__":
-    print("🚀 Server is running on http://0.0.0.0:5000")
+    print("🚀 Server is running on http://0.0.0.0:5001")
     # threads=6 handles multiple clan members clicking at once
-    serve(app, host='0.0.0.0', port=5000, threads=6)
+    serve(app, host='0.0.0.0', port=5001, threads=6)
+

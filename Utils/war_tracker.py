e642091ea004753f1f800e0fd3cd585c1400a55a
Tamer Vassib <tvassib@gmail.com>
2025-11-22 15:55:03 +0000

    Parent Dir change

--- Utils/war_tracker.py
+++ Utils/war_tracker.py
@@ -1,5 +1,9 @@
 import asyncio
 import coc
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from config import Config
 from sqlManager import SQLManager
 
